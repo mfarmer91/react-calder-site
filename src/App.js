17ba76ad2ab91e01e86fb22d55cb9b068b00366c
Michael Farmer <mfarmer91@gmail.com>
2017-09-12 20:34:40 -0400

    added guache and mobile images. created rough mobile version.

--- src/App.js
+++ src/App.js
@@ -24,12 +24,54 @@ const Home = () => (
 );
 
 const Guache = () => (
-    <div id='banner_two' className='container'>
+    <div id='guache_ctn'>
+        <div id='initial_guache' className='ui segment guache_segment'>
+            <img className="ui centered large bordered image" src={require("./images/guache_one.png")} />
+            <p className='guache_text'>Six White Orbs, 1975</p>
+        </div>
+
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/guache_two.png")} />
+            <p className='guache_text'>Untitled, 1953</p>                    
+        </div>
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/guache_three.png")} />
+            <p className='guache_text'>Pyramids, 1971</p>                    
+        </div>
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/guache_four.png")} />
+            <p className='guache_text'>UBU, 1971</p>                    
+        </div>
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/Calder.png")} />
+            <p className='guache_text'>Spotted Orb and Pyramids, 1956</p>                    
+        </div>
     </div>
 );
 
 const Mobile = () => (
-    <div id='banner_two' className='container'>
+     <div>
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/mobile_one.png")} />
+            <p className='mobile_text'>Made in Birmingham.</p>
+        </div>
+
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/mobile_two.png")} />
+            <p className='mobile_text'>Made in Birmingham.</p>
+        </div>
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/mobile_three.png")} />
+            <p className='mobile_text'>Made in Birmingham.</p>
+        </div>
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/mobile_four.png")} />
+            <p className='mobile_text'>Made in Birmingham.</p>
+        </div>
+        <div className='ui segment'>
+            <img className="ui centered large bordered image" src={require("./images/mobile_five.png")} />
+            <p className='mobile_text'>Made in Birmingham.</p>
+        </div>
     </div>
 );
 
@@ -39,7 +81,7 @@ class App extends Component {
             <div>
                 <div id='nav' className="ui inverted menu">
                   <a className="item" href='/home'>
-                    App
+                    Home
                   </a>
                   <a className="item" href='/guache'>
                     Guache
@@ -64,30 +106,31 @@ class App extends Component {
                 <Route path='/mobile' component={Mobile} />
                 <Route path='/guache' component={Guache} />
                 </div>
+            <Footer />
             </div>
         );
     }
 }
 
-//class Footer extends Component {
-//    
-//    onGitClick() {
-//        window.location.href="http://www.github.com/mfarmer91";
-//    }
-//    
-//    render() {
-//        return (
-//            <div className='footer'>
-//                 <div id='footer_ctn'>
-//                    <button className="ui center aligned basic button" onClick={this.onGitClick}>
-//                        <p className='footer_text'><i className="spy icon"></i>2017 Michael Farmer</p>
-//                        <p className='footer_text'>Made in London.</p>
-//                    </button>  
-//                </div>
-//            </div>
-//        );
-//    }
-//}
+class Footer extends Component {
+    
+    onGitClick() {
+        window.location.href="http://www.github.com/mfarmer91";
+    }
+    
+    render() {
+        return (
+            <div className='footer'>
+                 <div id='footer_ctn'>
+                    <button className="ui center aligned basic button" onClick={this.onGitClick}>
+                        <p className='footer_text'><i className="spy icon"></i>2017 Michael Farmer</p>
+                        <p className='footer_text'>Made in London.</p>
+                    </button>  
+                </div>
+            </div>
+        );
+    }
+}
 
 
 export default App;
