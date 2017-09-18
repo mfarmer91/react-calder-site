084a1d150cb0f7de108990ae77dfb00c0f3b449d
Michael Farmer <mfarmer91@gmail.com>
2017-09-18 17:09:36 -0400

    added about page + styled (somewhat) contact page

--- src/App.js
+++ src/App.js
@@ -115,6 +115,19 @@ const Contact = () => (
     </div>
 )
 
+const About = () => (
+    <div>
+        <div id='about_text' className="ui piled segment">
+  <h4 id='about_header' className="ui medium header">We know Calder.</h4>
+  <p>Love for Alexander Calder began at kinetic.  Calder Associates Inc. founder Michael Farmer experienced his first mobile — movable drawing in space — at the Virginia Museum of Fine Arts in Richmond, Virginia at 10 years old.  Fast-forward 15 years, and Michael has dedicated both academic study and 6 years art world employment to gaining an unparalleled expertise in Calder’s works.</p>
+   <p>Calder Associates Inc. offers certified authentication, in-depth price evaluation and international private sale of all Alexander Calder works.  In addition, a global network of renowned art-world connections — including museums, conservationists, collectors and auction houses — are at the disposal of any Calder Associates Inc. client.  Calder works move, and so does Calder’s market.  We know both.</p>
+   <p>Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores adolescens, no illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete utroque quo.</p>
+        </div>
+    </div>
+        
+)
+
+
 
 class App extends Component {
     
@@ -174,6 +187,8 @@ class App extends Component {
                 <Route path='/mobile' component={Mobile} />
                 <Route path='/gauche' component={Guache} />
                 <Route path='/contact' component={Contact} />
+                <Route path='/about' component={About} />
+
 
                 </div>
             <Footer />
@@ -216,7 +231,7 @@ class Footer extends Component {
                         <ul className='footer_list'>
                             <li><a href='/contact'>contact us</a></li>
                             <li><a>press</a></li>
-                            <li><a>about</a></li>
+                            <li><a href='/about'>about</a></li>
                         </ul>
                     </div>
                 </div>
